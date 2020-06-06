e9fc8c284992bea65ccb0cc597473c424fed81a9
azmalent <dmitrysjestov@niuitmo.ru>
2020-06-07 01:12:46 +0300

    Increment mod version

--- src/main/kotlin/azmalent/backportedflora/BackportedFlora.kt
+++ src/main/kotlin/azmalent/backportedflora/BackportedFlora.kt
@@ -24,7 +24,7 @@ import java.io.File
 object BackportedFlora {
     const val MODID = "backportedflora"
     const val NAME = "Backported Flora"
-    const val VERSION = "1.1"
+    const val VERSION = "1.2"
     const val CONFIG_FILE_NAME = MODID + ".cfg"
 
     const val ADAPTER = "net.shadowfacts.forgelin.KotlinAdapter"
